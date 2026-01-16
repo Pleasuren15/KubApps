627a2d08487fdc19684fd7ee19bff0798627a106
Pleasure Ndhlovu <pleasuren15@gmail.com>
2026-01-16 21:27:16 +0200

    shadcd & tailwind added under kubappsui

--- src/kubapps.ui/src/App.tsx
+++ src/kubapps.ui/src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <div className="items-center">
       <Button>Click me</Button>
     </div>
   )
