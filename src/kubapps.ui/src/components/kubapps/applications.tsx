30fba8c7076a6bff0b4e500f8edb138b3b054a51
Pleasure Ndhlovu <pleasuren15@gmail.com>
2026-01-18 11:09:45 +0200

    scroll area added

--- src/kubapps.ui/src/components/kubapps/applications.tsx
+++ src/kubapps.ui/src/components/kubapps/applications.tsx
@@ -1,10 +1,9 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, } from "@/components/ui/accordion"
 import { Badge } from "@/components/ui/badge"
-import { Separator } from "@radix-ui/react-select"
 import { ButtonGroup } from "@/components/ui/button-group"
 import { Button } from "@/components/ui/button"
 import { HoverCard, HoverCardContent, HoverCardTrigger, } from "@/components/ui/hover-card"
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger, } from "@/components/ui/drawer"
+import { Drawer, DrawerClose, DrawerContent, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger, } from "@/components/ui/drawer"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 
 const podDetails = [
@@ -29,13 +28,13 @@ function Application() {
                 <HoverCard>
                     <HoverCardTrigger asChild>
                         <div className="flex justify-between">
-                            <p className="text-l font-semibold flex justify-between">Labels(hover on link)</p>
+                            <p className="text-l font-semibold flex justify-between">Labels</p>
                             <p className="text-l font-semibold flex justify-between">
                                 <Badge className="mt-1 h-5 min-w-5 rounded-full px-1 font-mono tabular-nums" variant="outline">17+</Badge>
                             </p>
                         </div>
                     </HoverCardTrigger>
-                    <HoverCardContent className="w-80 bg-gray-600">
+                    <HoverCardContent className="w-80 bg-blue-600">
                         <div className="flex justify-between gap-4">
                             <div className="space-y-1">
                                 <Badge className="mr-1" variant="secondary">nginx</Badge>
@@ -53,25 +52,33 @@ function Application() {
                 <ButtonGroup className="w-full flex">
                     <Drawer>
                         <DrawerTrigger asChild>
-                            <Button className="flex-1 cursor-pointer" variant="outline">
+                            <Button className="flex-1 cursor-pointer text-left" variant="outline">
                                 View Logs
                             </Button>
                         </DrawerTrigger>
+
                         <DrawerContent>
-                            <div className="mx-auto">
-                                <DrawerHeader>
-                                    <DrawerTitle>Move Goal</DrawerTitle>
-                                    <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+                            <div className="w-full px-4">
+                                <DrawerHeader className="mx-auto w-3/4 text-left px-0">
+                                    <DrawerTitle className="text-left">Pod Logs: pod-gsayudkg765akj</DrawerTitle>
                                 </DrawerHeader>
-                                <DrawerFooter>
+                                <ScrollArea className="mx-auto w-3/4 rounded-md border">
+                                    <div className="p-4">
+                                        <h4 className="text-sm leading-none font-medium">Connected to database</h4>
+                                        <h4 className="text-sm leading-none font-medium">Connected to database</h4>
+                                        <h4 className="text-sm leading-none font-medium">Request received: GET /health</h4>
+                                        <h4 className="text-sm leading-none font-medium">Starting server on port 8080</h4>
+                                        <h4 className="text-sm leading-none font-medium">Request received: GET /health</h4>
+                                    </div>
+                                </ScrollArea>
+                                <DrawerFooter className="mx-auto w-3/4 flex justify-start px-0">
                                     <DrawerClose asChild>
-                                        <Button variant="destructive">Cancel</Button>
+                                        <Button variant="destructive" size="sm" className="cursor-pointer w-fit px-4">Close Logs</Button>
                                     </DrawerClose>
                                 </DrawerFooter>
                             </div>
                         </DrawerContent>
                     </Drawer>
-
                     <Button className="flex-1 cursor-pointer bg-blue-600 hover:bg-blue-700">
                         Forward Pod
                     </Button>
@@ -84,6 +91,12 @@ function Application() {
 function Applications() {
     return (
         <div className="mx-auto max-w-7xl mt-3 grid grid-cols-4 gap-4">
+            <Application />
+            <Application />
+            <Application />
+            <Application />
+            <Application />
+            <Application />
             <Application />
             <Application />
             <Application />
